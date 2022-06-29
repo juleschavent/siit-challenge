2d77a2e4c18940f35317fed2cac503adc7b9ca2c
jules <juleschavent@gmail.com>
2022-06-29 12:20:05 +0200

    setup project architecture with react

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    semi: ['error', 'never'],
+    'no-unused-vars': 'warn',
+    'react/destructuring-assignment': 1,
+    'react/prop-types': 1,
+    'import/prefer-default-export': 'off',
+  },
+}
