a57726d0a2b7ccbac1094f493470cee2f79f508b
jules <juleschavent@gmail.com>
2022-06-29 16:23:36 +0200

    lint fix on server

--- server/index.js
+++ server/index.js
@@ -1,44 +1,48 @@
-const express = require("express");
-const services = require("./services.json");
-const users = require("./users.json");
-const app = express();
-const Bundler = require("parcel-bundler");
-const bundler = new Bundler("src/index.html", { logLevel: 2 });
+const express = require('express')
+const services = require('./services.json')
+const users = require('./users.json')
 
-const PORT = 3001;
+const app = express()
+const Bundler = require('parcel-bundler')
+
+const bundler = new Bundler('src/index.html', { logLevel: 2 })
+
+const PORT = 3001
 
 app.get('/services.json', (req, res) => {
+  // The following works locally
   res.header('Access-Control-Allow-Origin', '*')
-  res.json(services);
-});
+  res.json(services)
+})
 
 app.get('/users.json', (req, res) => {
   let filteredUsers = users.map((user, i) => {
-    const id = i + 1;
-    return { id, avatar_url: `https://eu.ui-avatars.com/api/?name=${user.name}`, ...user };
-  });
+    const id = i + 1
+    return { id, avatar_url: `https://eu.ui-avatars.com/api/?name=${user.name}`, ...user }
+  })
 
   if (req.query.service_id) {
-    filteredUsers = filteredUsers.filter((u) => u.service_ids.includes(parseInt(req.query.service_id)));
+    filteredUsers = filteredUsers.filter((u) => u.service_ids.includes(parseInt(req.query.service_id)))
   }
+  // The following doesn't works locally
   res.header('Access-Control-Allow-Origin', '*')
-  res.json(filteredUsers);
+  res.json(filteredUsers)
 })
 
-app.use(express.static("server/public"));
-app.use(bundler.middleware());
+app.use(express.static('server/public'))
+app.use(bundler.middleware())
 
 app.listen(PORT, () => {
   // Clear console
   process.stdout.write(
-    process.platform === "win32" ? "\x1B[2J\x1B[0f" : "\x1B[2J\x1B[3J\x1B[H"
-    );
+    process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H',
+  )
 
-  console.log("\x1b[32m%s\x1b[0m", "App started successfully!");
-  console.log();
-  console.log("You can now view it in your browser.");
-  console.log();
-  console.log(`  http://localhost:${PORT}`);
-  console.log();
-  console.log("You'll find more instruction in the README file.");
-});
+  console.log('\x1b[32m%s\x1b[0m', 'App started successfully!')
+  console.log()
+  console.log('You can now view it in your browser.')
+  console.log()
+  console.log(`  http://localhost:${PORT}`)
+  console.log()
+  console.log("You'll find more instruction in the README file.")
+})
