8e98ed6bccc862f5fd5952b402104194c20e84d4
jules <juleschavent@gmail.com>
2022-06-30 18:26:07 +0200

    fix undefined again

--- src/utils/UseUserPerService.js
+++ src/utils/UseUserPerService.js
@@ -2,17 +2,22 @@ import { useContext, useEffect, useState } from 'react'
 import { StoreContext } from '../context/store'
 
 export const useUserPerService = (id) => {
-  const { userData } = useContext(StoreContext)
+  const { userData, userLoading } = useContext(StoreContext)
   const [nbUser, setNbUser] = useState(0)
 
   useEffect(() => {
     let tempUser = 0
-    userData.forEach((user) => {
-      if (user.service_ids.includes(id)) {
-        tempUser += 1
-      }
-    })
+    if (userLoading) {
+      setNbUser(0)
+    } else if (userData) {
+      userData.forEach((user) => {
+        if (user.service_ids.includes(id)) {
+          tempUser += 1
+        }
+      })
+    }
     setNbUser(tempUser)
-  }, [userData])
+  }, [userData, userLoading])
+
   return nbUser
 }
